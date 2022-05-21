f5bde37e4f1244fdbece864b9b691b7920ac86fe
hehongbo <hhbjkd@163.com>
2022-05-21 08:30:39 +0800

    单机爬虫增加翻页数据获取

--- SingleTask/zhenai/parser/citylist.go
+++ SingleTask/zhenai/parser/citylist.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"GoSpider/SingleTask/engine"
 	"regexp"
+	"strconv"
 )
 
 var CityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
@@ -11,21 +12,26 @@ func ParseCityList(contens []byte) engine.ParseResult {
 	re := regexp.MustCompile(CityListRe)
 	matches := re.FindAllSubmatch(contens,-1)
 	result := engine.ParseResult{}
-	limit := 10
+	//limit := 1
+	page_limit := 6 // 页面限制数据为 6 暂时取 6页 数据
 	for _, m := range matches {
 		result.Items = append(result.Items,"City " +string(m[2]))
-		result.Requests = append(result.Requests,engine.Request{
-			Type: "url",
-			Url: string(m[1]),
-			//ParserFuc: engine.NilParser,
-			ParserFuc: func(bytes []byte) engine.ParseResult {
-				return ParseCityUserList(bytes, string(m[1]))
-			},
-		})
-		limit --
-		if limit == 0 {
-			break
+		// 追加页面翻页数据请求
+		for i :=1; i <= page_limit; i ++ {
+			url := string(m[1]) + "/" + strconv.Itoa(i)
+			result.Requests = append(result.Requests,engine.Request{
+				Type: "url",
+				Url: url,
+				ParserFuc: func(bytes []byte) engine.ParseResult {
+					return ParseCityUserList(bytes, url)
+				},
+			})
 		}
+
+		//limit --
+		//if limit == 0 {
+		//	break
+		//}
 	}
 	return result
 }
